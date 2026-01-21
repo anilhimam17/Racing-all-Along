9ee636c90ae5df3c3e3a54e9cfbcc8d5e5685282
Purple Lightning <anilhimamg@gmail.com>
2026-01-21 20:49:14 +0000

    feat: added utils.py with driver number map

--- F1/src/setup.py
+++ F1/src/setup.py
@@ -4,4 +4,3 @@ from pathlib import Path
 # Global Paths consistent for each race weekend
 DATA_PATH = Path("data")
 DATA_PATH.mkdir(exist_ok=True)
-
---
+++ F1/src/utils.py
@@ -0,0 +1,23 @@
+# Driver Number to Short Name mapping for downstream tasks.
+DRIVER_NUMBER_MAP = {
+    "1": "VER",
+    "81": "PIA",
+    "63": "RUS",
+    "16": "LEC",
+    "12": "ANT",
+    "55": "SAI",
+    "44": "HAM",
+    "22": "TSU",
+    "10": "GAS",
+    "4": "NOR",
+    "23": "ALB",
+    "30": "LAW",
+    "14": "ALO",
+    "6": "HAD",
+    "87": "BEA",
+    "18": "STR",
+    "7": "DOO",
+    "27": "HUL",
+    "31": "OCO",
+    "5": "BOR"
+}
