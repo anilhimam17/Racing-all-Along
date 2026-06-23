4b09c16612c854b23ad1a1b1a1a2694ba905a8c9
anilhimam17 <anilhimamg@gmail.com>
2026-06-23 08:26:46 +0100

    feat: new v1 Params and Configs

---
+++ F1/src/v1/config.py
@@ -0,0 +1,29 @@
+# ======================= Assumed Constant for Pace Grounding =======================
+# Fuel Load
+MAX_FUEL_LOAD_IN_KG = 70
+
+# Fuel Flow
+MAX_FUEL_FLOW_IN_KGH = 100
+FUEL_FLOW_CONV_CONST = 1000 / 3600
+
+# Fuel Save for Race Trim pace
+FUEL_STRAT = 10
+
+# Car Weight without Fuel with Driver (assumed optimal)
+CAR_WEIGHT_IN_KG = 772
+
+# Weight to Time Conversion => 0.3s every 10kg
+WEIGHT_TIME_CONV_CONST = 0.3 / 10
+
+# Considering Hybrid as a 48% split
+HYBRID_POWER = 0.4
+
+# Fuel Sample Limit
+FUEL_SAMPLE_LIMIT = 3
+
+# ======================= Circuit Specific Configurations =======================
+
+# Conservatively 70% Full Throttle
+THROTTLE_CYCLE = {
+    "barcelona": 0.7
+}
