27b7a7a78b34c4a0fa43b90c58332d568eec8b08
anilhimam17 <anilhimamg@gmail.com>
2026-06-24 23:58:02 +0100

    refactor: updated the values for fuel strat and added new constant straights

--- F1/src/v1/config.py
+++ F1/src/v1/config.py
@@ -7,7 +7,7 @@ MAX_FUEL_FLOW_IN_KGH = 100
 FUEL_FLOW_CONV_CONST = 1000 / 3600
 
 # Fuel Save for Race Trim pace
-FUEL_STRAT = 10
+FUEL_STRAT = 5
 
 # Car Weight without Fuel with Driver (assumed optimal)
 CAR_WEIGHT_IN_KG = 772
@@ -16,14 +16,19 @@ CAR_WEIGHT_IN_KG = 772
 WEIGHT_TIME_CONV_CONST = 0.3 / 10
 
 # Considering Hybrid as a 48% split
-HYBRID_POWER = 0.4
+HYBRID_POWER = 0.47
 
 # Fuel Sample Limit
 FUEL_SAMPLE_LIMIT = 3
 
 # ======================= Circuit Specific Configurations =======================
 
-# Conservatively 70% Full Throttle
+# The Percentage of Time on Full Throttle by Circuit
 THROTTLE_CYCLE = {
     "barcelona": 0.7
+}
+
+# The Distance the Longest Straight for a Given Circuit
+STRAIGHTS = {
+    "barcelona": 0.44   # in KM by Al Kamel systems
 }
