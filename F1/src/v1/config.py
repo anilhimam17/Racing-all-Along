52559a194dbabd59bb6bf7e3a638333e38e43d2d
anilhimam17 <anilhimamg@gmail.com>
2026-07-02 17:33:47 +0100

    refactor: added standard dataclasses for all configurations

--- F1/src/v1/config.py
+++ F1/src/v1/config.py
@@ -1,62 +1,139 @@
-# ======================= Assumed Constant for Pace Grounding =======================
-# Fuel Load
-MAX_FUEL_LOAD_IN_KG = 70
+from dataclasses import dataclass, field
 
-# Fuel Flow
-MAX_FUEL_FLOW_IN_KGH = 100
-FUEL_FLOW_CONV_CONST = 1000 / 3600
 
-# Fuel Save for Race Trim pace
-FUEL_STRAT = 5
+# ======================= Generic Mapping =======================
 
-# Car Weight without Fuel with Driver (assumed optimal)
-CAR_WEIGHT_IN_KG = 772
-
-# Weight to Time Conversion => 0.3s every 10kg
-WEIGHT_TIME_CONV_CONST = 0.3 / 10
-
-# KM-HR to M-S Time Conversion
-MS_CONV_CONST = 5 / 18
-
-# Considering Hybrid as a 48% split
-HYBRID_POWER = 0.48
-
-# Fuel Sample Limit
-FUEL_SAMPLE_LIMIT = 3
-
-# ======================= Circuit Specific Configurations =======================
-
-# The Distance the Longest Straight for a Given Circuit
-STRAIGHTS = {
-    "barcelona": 0.44   # in KM by Al Kamel systems
-}
-
-# ======================= Data Visualisation Configurations =======================
-
-SECTOR_MAPS = {
+SECTOR_MAPS: dict[str, tuple[str, str, str]] = {
     "Sector1": ("SpeedI1", "Sector1Time", "KineticEnergyS1_KJ"),
     "Sector2": ("SpeedI2", "Sector2Time", "KineticEnergyS2_KJ"),
     "Sector3": ("SpeedFL", "Sector3Time", "KineticEnergyS3_KJ")
 }
 
-AERO_VIS_CONFIG = [
-    ("Sector1Time", "FrontAEI", "Sector 1\nHigh Speed, Downforce / Drag Tradeoff"), 
-    ("Sector2Time", "BalancedAEI", "Sector 2\nMedium Speed, High Downforce & Minimal Drag"), 
-    ("Sector3Time", "RearAEI", "Sector 3\nMedium - Low Speed, High Downforce")
-]
+# ======================= Feature / Data Configurations =======================
 
-ERS_VIS_CONFIG = [
-    ("AccelerationTime", "ERS_Clipping", "")
-]
 
-KE_VIS_CONFIG = [
-    ("Driver", "KineticEnergyS1_KJ", "Sector 1\nHigh Speed, Downforce / Drag Tradeoff"),
-    ("Driver", "KineticEnergyS2_KJ", "Sector 2\nMedium Speed, High Downforce & Minimal Drag"),
-    ("Driver", "KineticEnergyS3_KJ", "Sector 3\nMedium - Low Speed, High Downforce"),
-]
+@dataclass(frozen=True)
+class FeatureConfig:
 
-POWER_VIS_CONFIG = [
-    ("Driver", "PowerS1_KW", "Sector 1\nHigh Speed, Downforce / Drag Tradeoff"),
-    ("Driver", "PowerS2_KW", "Sector 2\nMedium Speed, High Downforce & Minimal Drag"),
-    ("Driver", "PowerS3_KW", "Sector 3\nMedium - Low Speed, High Downforce")
-]
+    # ======================= Feature Categorisation =======================
+    # Overlapping Features, especially for this regulations
+    COMMON_CATEGORIES: list[str] = field(default_factory=lambda: [
+        "AccelerationTime", "ERS_Clipping"
+    ])
+
+    # Pace Specific Categories
+    PACE_CATEGORIES: list[str] = field(default_factory=lambda: [
+        "LapTime", "Sector1Time", "Sector2Time", "Sector3Time", 
+        *FeatureConfig.COMMON_CATEGORIES
+    ])
+
+    # Speed Specific Categories
+    SPEED_CATEGORIES: list[str] = field(default_factory=lambda: [
+        "SpeedI1", "SpeedI2", "SpeedFL", "SpeedST",
+        "FrontAEI", "BalancedAEI", "RearAEI", 
+        *FeatureConfig.COMMON_CATEGORIES, FeatureConfig.PACE_CATEGORIES[0]
+    ])
+
+    # Energy Specific Categories
+    ENERGY_CATEGORIES: list[str] = field(default_factory=lambda: [
+        "KineticEnergyS1_KJ", "KineticEnergyS2_KJ", "KineticEnergyS3_KJ",
+        *FeatureConfig.COMMON_CATEGORIES, FeatureConfig.PACE_CATEGORIES[0]
+    ])
+
+    # ======================= Feature Scaling Properties =======================
+    DIRECT_PROPORTION: list[str] = field(default_factory=lambda: [
+        "SpeedI1", "SpeedI2", "SpeedFL", "SpeedST", 
+        "FrontAEI", "BalancedAEI", "RearAEI"
+    ])
+
+    INVERSE_PROPORTION: list[str] = field(default_factory=lambda: [
+        "Sector1Time", "Sector2Time", "Sector3Time", *FeatureConfig.ENERGY_CATEGORIES
+    ])
+
+
+# ======================= Assumed Constant for Pace Grounding =======================
+
+
+@dataclass(frozen=True)
+class CarSepecifications:
+    """This class is the container for all the car specifications."""
+
+    # Fuel Load
+    MAX_FUEL_LOAD_IN_KG: float = 70
+
+    # Car Weight without Fuel with Driver (assumed optimal)
+    CAR_WEIGHT_IN_KG: float = 772
+    
+    # Fuel Flow
+    MAX_FUEL_FLOW_IN_KGH: float = 100
+    
+    # Considering Hybrid as a 48% split
+    HYBRID_POWER: float = 0.48
+
+
+@dataclass(frozen=True)
+class ConversionConstants:
+    """This class is the container for all the conversion constants relevant to the
+    car performance calculations."""
+
+    # Fuel Flow in g/sec
+    FUEL_FLOW_CONV_CONST = 1000 / 3600
+
+    # Weight to Time Conversion => 0.3s every 10kg
+    WEIGHT_TIME_CONV_CONST = 0.3 / 10
+
+    # KM-HR to M-S Time Conversion
+    MS_CONV_CONST = 5 / 18
+    
+    
+@dataclass(frozen=True)
+class RaceStrategyConfig:
+    """This class is the container for all the race specific configurations."""
+
+    # Fuel Save for Race Trim pace
+    FUEL_STRAT = 5
+
+    # Fuel Sample Limit
+    FUEL_SAMPLE_LIMIT = 3
+
+
+# ======================= Circuit Specific Configurations =======================
+
+
+@dataclass(frozen=True)
+class CircuitData:
+    """This class is the container for all the circuit specification in the Grandprix Calender."""
+
+    STRAIGHTS: dict[str, float] = field(default_factory=lambda: {
+        "barcelona": 0.44   # Verified by Al Kamel
+    })
+
+
+# ======================= Data Visualisation Configurations =======================
+
+
+@dataclass(frozen=True)
+class VisualisationConfig:
+    """This class if the container for all the Visualisation based configurations."""
+
+    AERO_VIS_CONFIG: list[tuple[str, str, str]] = field(default_factory=lambda: [
+        ("Sector1Time", "FrontAEI", "Sector 1\nHigh Speed, Downforce / Drag Tradeoff"), 
+        ("Sector2Time", "BalancedAEI", "Sector 2\nMedium Speed, High Downforce & Minimal Drag"), 
+        ("Sector3Time", "RearAEI", "Sector 3\nMedium - Low Speed, High Downforce")
+    ])
+
+    ERS_VIS_CONFIG: list[tuple[str, str, str]] = field(default_factory=lambda: [
+        ("AccelerationTime", "ERS_Clipping", "")
+    ])
+
+    KE_VIS_CONFIG: list[tuple[str, str, str]] = field(default_factory=lambda: [
+        ("Driver", "KineticEnergyS1_KJ", "Sector 1\nHigh Speed, Downforce / Drag Tradeoff"),
+        ("Driver", "KineticEnergyS2_KJ", "Sector 2\nMedium Speed, High Downforce & Minimal Drag"),
+        ("Driver", "KineticEnergyS3_KJ", "Sector 3\nMedium - Low Speed, High Downforce"),
+    ])
+
+    POWER_VIS_CONFIG: list[tuple[str, str, str]] = field(default_factory=lambda: [
+        ("Driver", "PowerS1_KW", "Sector 1\nHigh Speed, Downforce / Drag Tradeoff"),
+        ("Driver", "PowerS2_KW", "Sector 2\nMedium Speed, High Downforce & Minimal Drag"),
+        ("Driver", "PowerS3_KW", "Sector 3\nMedium - Low Speed, High Downforce")
+    ])
