b312a6a4f3d079488fd366d6221fd93dcf8f0b57
anilhimam17 <anilhimamg@gmail.com>
2026-07-03 19:58:08 +0100

    feat: added create_comparison_radar method and scaffolded code for new config dataclasses

--- F1/src/v1/visualisation.py
+++ F1/src/v1/visualisation.py
@@ -1,23 +1,25 @@
 # FastF1 Deps
 from fastf1.core import Session
 
-from fastf1.plotting import get_driver_color_mapping
-
 # Data Deps
 from pandas import DataFrame
+from numpy import append
 
 # Visualisation Deps
+from fastf1.plotting import get_driver_color_mapping
+
 import matplotlib.pyplot as plt
+from matplotlib.colors import to_rgba
 from matplotlib import figure
+
 import seaborn as sns
 
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from plotly.offline import init_notebook_mode
+
 # Source Deps
-from src.v1.config import (
-    AERO_VIS_CONFIG,
-    ERS_VIS_CONFIG,
-    KE_VIS_CONFIG,
-    POWER_VIS_CONFIG
-)
+from src.v1.config import VisualisationConfig, FeatureConfig
 
 
 class DataVisualisation:
@@ -33,10 +35,24 @@ class DataVisualisation:
         # Accessing all the Driver Colors
         driver_colors_full = get_driver_color_mapping(session=session)
         
-        # Cacheing the Necessary Driver Colors
-        self.driver_color = {}
+        # Cacheing the Necessary Driver Colors in Hexcode
+        self.driver_color_hex = {}
         for driver in driver_names:
-            self.driver_color[driver] = driver_colors_full[driver]
+            self.driver_color_hex[driver] = driver_colors_full[driver]
+
+        # Cacheing the Necessary Driver Colors as RGBA
+        self.driver_color_rgba = {}
+        for driver in driver_names:
+            
+            # Converting the Hexcode to RGBA
+            rgba = to_rgba(driver_colors_full[driver], alpha=0.5)
+            rgba_str = f"rgba({rgba[0]}, {rgba[1]}, {rgba[2]}, {rgba[3]})"
+            
+            self.driver_color_rgba[driver] = rgba_str
+
+        # Instance of all the necessary configurations
+        self.vis_config = VisualisationConfig()
+        self.feature_config = FeatureConfig()
 
     # ======================= Member Methods =======================
     def create_scatter_plots(
@@ -68,9 +84,9 @@ class DataVisualisation:
         # Accessing the Scatter Plot Configurations
         plot_configs = None
         if plot_kind == "aero":
-            plot_configs = AERO_VIS_CONFIG
+            plot_configs = self.vis_config.AERO_VIS_CONFIG
         elif plot_kind == "ers_clip":
-            plot_configs = ERS_VIS_CONFIG
+            plot_configs = self.vis_config.ERS_VIS_CONFIG
 
         # Sanity Check for Plot Configs
         assert plot_configs, "The plot configuration wasn't satisfied."
@@ -85,7 +101,7 @@ class DataVisualisation:
                 x=x,
                 y=y,
                 hue=hue,
-                palette=self.driver_color,
+                palette=self.driver_color_hex,
                 ax=axes,
                 s=size
             )
@@ -102,7 +118,7 @@ class DataVisualisation:
                     x=x,
                     y=y,
                     hue=hue,
-                    palette=self.driver_color,
+                    palette=self.driver_color_hex,
                     ax=axes[ax_idx],
                     s=size
                 )
@@ -136,9 +152,9 @@ class DataVisualisation:
         # Accessing the Scatter Plot Configurations
         plot_configs = None
         if plot_kind == "ke":
-            plot_configs = KE_VIS_CONFIG
+            plot_configs = self.vis_config.KE_VIS_CONFIG
         elif plot_kind == "power":
-            plot_configs = POWER_VIS_CONFIG
+            plot_configs = self.vis_config.POWER_VIS_CONFIG
 
         # Sanity Check for Plot Configs
         assert plot_configs, "The plot configuration wasn't satisfied."
@@ -151,7 +167,7 @@ class DataVisualisation:
                 x=x,
                 y=y,
                 hue=hue,
-                palette=self.driver_color,
+                palette=self.driver_color_hex,
                 ax=axes[ax_idx],
             )
             
@@ -185,7 +201,7 @@ class DataVisualisation:
             col=col,
             height=height,
             aspect=aspect,
-            palette=self.driver_color
+            palette=self.driver_color_hex
         )
 
         # Plotting the Facets with Regplots
@@ -228,7 +244,7 @@ class DataVisualisation:
             y=y,
             ax=axes,
             hue=hue,
-            palette=self.driver_color,
+            palette=self.driver_color_hex,
             gap=0.2
         )
 
@@ -237,3 +253,222 @@ class DataVisualisation:
         axes.grid()
 
         return pace_grid
+    
+    def create_comparison_radar(
+            self,
+            quali_performance: DataFrame,
+            race_performance: DataFrame,
+            drivers: list,
+            show_pace_categories: bool = True,
+            show_speed_categories: bool = True,
+            show_energy_categories: bool = True
+        ) -> go.Figure:
+        """This function generates a Plotly subplot that visualises multiple
+        Radar Plots (go.Scatterpolar objects) which compare the performances of 
+        the drivers in quali and race trim."""
+
+        # Subplot Spec
+        ncols = 2
+        nrows = [
+            show_pace_categories, 
+            show_energy_categories, 
+            show_speed_categories
+        ].count(True)
+        row_idx = [i for i in range(1, nrows + 1)]
+
+        energy_categories_closed = None
+        pace_categories_closed = None 
+        speed_categories_closed = None
+        subplot_titles = []
+        row_track = 0
+        energy_row_index = None
+        pace_row_index = None
+        speed_row_index = None
+        
+        if show_pace_categories:
+            subplot_titles.extend(["Pace: Quali", "Pace: Race"])
+            pace_categories_closed = (
+                self.feature_config.PACE_CATEGORIES + 
+                [self.feature_config.PACE_CATEGORIES[0]]
+            )
+            pace_row_index = row_idx[row_track]
+            row_track += 1
+        if show_speed_categories:
+            subplot_titles.extend(["Speed: Quali", "Speed: Race"])
+            speed_categories_closed = (
+                self.feature_config.SPEED_CATEGORIES +
+                [self.feature_config.SPEED_CATEGORIES[0]]
+            )
+            speed_row_index = row_idx[row_track]
+            row_track += 1
+        if show_energy_categories:
+            subplot_titles.extend(["Energy: Quali", "Energy: Race"])
+            energy_categories_closed = (
+                self.feature_config.ENERGY_CATEGORIES + 
+                [self.feature_config.ENERGY_CATEGORIES[0]]
+            )
+            energy_row_index = row_idx[row_track]
+            row_track += 1
+
+        # Plotly Subplots Canvas
+        fig = make_subplots(
+            rows=nrows, 
+            cols=ncols, 
+            shared_xaxes=False,
+            shared_yaxes=False,
+            subplot_titles=subplot_titles,
+            specs=[[{"type": "polar"}, {"type": "polar"}] for _ in range(nrows)],
+            vertical_spacing=0.1,
+            horizontal_spacing=0.3,
+        )
+
+        # Adding the Traces
+        for driver in drivers:
+            
+            # Accessing the Driver Data
+            driver_quali_data = quali_performance[quali_performance["Driver"] == driver]
+            driver_race_data = race_performance[race_performance["Driver"] == driver]
+
+            # ================ Pace Categories ================
+            if show_pace_categories:
+                r_quali_pace = driver_quali_data[self.feature_config.PACE_CATEGORIES].to_numpy().flatten()
+                r_quali_pace_closed = append(r_quali_pace, r_quali_pace[0])
+
+                r_race_pace = driver_race_data[self.feature_config.PACE_CATEGORIES].to_numpy().flatten()
+                r_race_pace_closed = append(r_race_pace, r_race_pace[0])
+
+                # Quali Trace
+                fig.add_trace(
+                    go.Scatterpolar(
+                        r=r_quali_pace_closed,
+                        theta=pace_categories_closed,
+                        fill="toself",
+                        name=driver,
+                        fillcolor=self.driver_color_rgba[driver],
+                        legendgrouptitle_text=driver,
+                        legendgroup=driver,
+                        subplot="polar",
+                        showlegend=True
+                    ),
+                    row=pace_row_index,
+                    col=1,
+                )
+                # Race Trace
+                fig.add_trace(
+                    go.Scatterpolar(
+                        r=r_race_pace_closed,
+                        theta=pace_categories_closed,
+                        fill="toself",
+                        name=driver,
+                        fillcolor=self.driver_color_rgba[driver],
+                        legendgrouptitle_text=driver,
+                        legendgroup=driver,
+                        subplot="polar2",
+                        showlegend=False
+                    ),
+                    row=pace_row_index,
+                    col=2
+                )
+            
+            # ================ Speed Categories ================
+            if show_speed_categories:
+                r_quali_speed = driver_quali_data[self.feature_config.SPEED_CATEGORIES].to_numpy().flatten()
+                r_quali_speed_closed = append(r_quali_speed, r_quali_speed[0])
+
+                r_race_speed = driver_race_data[self.feature_config.SPEED_CATEGORIES].to_numpy().flatten()
+                r_race_speed_closed = append(r_race_speed, r_race_speed[0])
+
+                # Quali Trace
+                fig.add_trace(
+                    go.Scatterpolar(
+                        r=r_quali_speed_closed,
+                        theta=speed_categories_closed,
+                        fill="toself",
+                        name=driver,
+                        fillcolor=self.driver_color_rgba[driver],
+                        legendgrouptitle_text=driver,
+                        legendgroup=driver,
+                        subplot="polar3",
+                        showlegend=False
+                    ),
+                    row=speed_row_index,
+                    col=1,
+                )
+                # Race Trace
+                fig.add_trace(
+                    go.Scatterpolar(
+                        r=r_race_speed_closed,
+                        theta=speed_categories_closed,
+                        fill="toself",
+                        name=driver,
+                        fillcolor=self.driver_color_rgba[driver],
+                        legendgrouptitle_text=driver,
+                        legendgroup=driver,
+                        subplot="polar4",
+                        showlegend=False
+                    ),
+                    row=speed_row_index,
+                    col=2
+                )
+            
+            # ================ Energy Categories ================
+            if show_energy_categories:
+                r_quali_energy = driver_quali_data[self.feature_config.ENERGY_CATEGORIES].to_numpy().flatten()
+                r_quali_energy_closed = append(r_quali_energy, r_quali_energy[0])
+
+                r_race_energy = driver_race_data[self.feature_config.ENERGY_CATEGORIES].to_numpy().flatten()
+                r_race_energy_closed = append(r_race_energy, r_race_energy[0])
+
+                # Quali Trace
+                fig.add_trace(
+                    go.Scatterpolar(
+                        r=r_quali_energy_closed,
+                        theta=energy_categories_closed,
+                        fill="toself",
+                        name=driver,
+                        fillcolor=self.driver_color_rgba[driver],
+                        legendgrouptitle_text=driver,
+                        legendgroup=driver,
+                        subplot="polar5",
+                        showlegend=False
+                    ),
+                    row=energy_row_index,
+                    col=1,
+                )
+                # Race Trace
+                fig.add_trace(
+                    go.Scatterpolar(
+                        r=r_race_energy_closed,
+                        theta=energy_categories_closed,
+                        fill="toself",
+                        name=driver,
+                        fillcolor=self.driver_color_rgba[driver],
+                        legendgrouptitle_text=driver,
+                        legendgroup=driver,
+                        subplot="polar6",
+                        showlegend=False
+                    ),
+                    row=energy_row_index,
+                    col=2
+                )
+
+        polar_configs = {}
+        for i in range(1, nrows * ncols):
+            if i == 1:
+                polar_configs["polar"] = self.vis_config.POLAR_CONFIG
+            else:
+                polar_configs[f"polar{i}"] = self.vis_config.POLAR_CONFIG
+
+        fig.update_layout(
+            showlegend=True,
+            height=int(nrows * 600),
+            width=1280,
+            title_text="Driver-Wise Performance Quali V Race",
+            title_x=0.5,
+            title_y=0.98,
+            legend=dict(x=1.1, y=0.5, xanchor="left", yanchor="middle"),
+            margin=dict(l=150, r=150, t=150, b=50),
+            **polar_configs
+        )
+
+        return fig
